1df0d5d9cebef7f2a6682b1cb9cf21abae0e84e4
Beau Hale <halebeau3@gmail.com>
2021-02-23 17:03:21 -0800

    first day commit

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <Container>
+            <Main>
+                <AccessTimeIcon />
+                <SearchContainer>
+                    <Search>
+                        <input type="text" placeholder="Search..." />
+                    </Search>
+                </SearchContainer>
+                <HelpOutlineIcon />
+            </Main>
+            <UserContainer>
+                <Name>
+                    Beau
+                </Name>
+                <UserImage>
+                    <img src="https://i.imgur.com/6VBx3io.png" />
+                </UserImage>
+            </UserContainer>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+background: #350d36;
+color: white;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+
+const Main = styled.div`
+    display: flex;
+`
+
+const SearchContainer = styled.div`
+`
+
+const Search = styled.div`
+`
+const UserContainer = styled.div`
+    display: flex;
+    align-items: center;
+`
+const Name = styled.div`
+`
+const UserImage = styled.div`
+`
