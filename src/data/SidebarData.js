858d8d6d281f9121f0985a8122cab66488ae588c
Beau Hale <halebeau3@gmail.com>
2021-02-23 20:24:01 -0800

    Completed Sidebar first day

---
+++ src/data/SidebarData.js
@@ -0,0 +1,34 @@
+import MessageIcon from '@material-ui/icons/Message';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+
+
+export const sidebarItemsData = [
+    {
+        icon: <MessageIcon />,
+        text: "Thread"
+    },
+    {
+        icon: <InboxIcon />,
+        text: "All DM's"
+    },
+    {
+        icon: <DraftsIcon />,
+        text: "Mentions & Reactions"
+    },
+    {
+        icon: <BookmarkBorderIcon />,
+        text: "Save Items"
+    },
+    {
+        icon: <PeopleAltIcon />,
+        text: "Peoples & Groups"
+    },
+    {
+        icon: <AppsIcon />,
+        text: "More"
+    }
+]
